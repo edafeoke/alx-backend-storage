867feb8ca6586727d3b5b792d40a2c1e208c2cb5
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-22 15:05:43 -0700

    Addd 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,12 +2,12 @@
 -- ComputeAverageScoreForUser that computes and store
 -- the average score for a student
 DELIMITER |
-CREATE PROCEDURE ComputeOverallScoreForUser (
+CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id int
 )
 BEGIN
     UPDATE users
-    SET overall_score = (
+    SET average_score = (
         SELECT AVG(score) FROM corrections
         WHERE corrections.user_id = user_id
         GROUP BY corrections.user_id
